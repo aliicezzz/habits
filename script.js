f11ddf277325186e5aa2d48780ea2edda09f9e52
Alice Ribeiro <aliceribeiro.work@gmail.com>
2023-01-25 23:03:02 -0300

    aula 4 atualizada

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+button.addEventListener('click', add)
+form.addEventListener('change, save')
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0,-5)
+  const dayExists =  nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert("Dia já incluso!")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetuo@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetuo@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
